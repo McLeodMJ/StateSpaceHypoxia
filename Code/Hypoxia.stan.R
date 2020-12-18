754cce02fffcde7c1cf7a33f8d6e6bcfa7a5967b
McleodMJ <montana.mcleod@oregonstate.edu>
2020-12-18 11:21:58 -0800

    fixed the likelihoods to swap occupied and detection probs.

--- Code/Hypoxia.stan.R
+++ Code/Hypoxia.stan.R
@@ -7,27 +7,14 @@
 ##########################################
 setwd("~/Box Sync/McLeod_thesis/code")
 library(rstan)
+library(bayesplot)
+library(shinystan)
+library('dplR')
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-#library("devtools")
-#devtools::install_github("andybunn/dplR")
-library('dplR')
-
-
 
 ############################################## Simulated Data ######################################################
-#load('Groundfish.Hyp.Rdata')
-#RUN CODE BELOW TO REPLACE - detrend(Dsole$o2_at_gear_ml_per_l_der) 
-#hypox <- lm(Lingcod$o2_at_gear_ml_per_l_der ~ Lingcod$year)$resid #Take the residuals of a stright line
-### Should we be taking the absolute value so we do not have negative DO concentrations?
-
-#z <- fft(hypox) #fourier transform
-### DO WE RUN A sample() FROM HERE?
-
-# Convert time to a more useful scale (proportions of a year):
-#Time.fft = (1:length(z)) / 12
-#plot(Time.fft, na.omit(z)^2, type='l', xlab='Frequency', ylab='Variance' ) #frequency is alrgely swayed to the left and right
 load("DO_sims.Rdata")
 
 good_yr <- DsD[DsD$Year == 2015,4]
@@ -42,7 +29,7 @@ inv_logit <- function(x){
 
 
 ############################################  Initial variables # OK NOW NEW VALUES FOR THESE THINGS? ######################
-N = length(bad_yr)
+N = length(good_yr)
 psi = 0.5 #occ.
 p = 0.2 #det.
 hypox.p = 1 # not sure what to do for this value which is used below in the inv-logit eqn but 1+ works
@@ -50,7 +37,7 @@ hypox.p = 1 # not sure what to do for this value which is used below in the inv-
 
 
 ############################################ factoring in hypoxia as a covariate ############################################ 
-psi.full <- inv_logit(logit(psi) + hypox.p * bad_yr)
+psi.full <- inv_logit(logit(psi) + hypox.p * good_yr)
 ## Why inverset then logit fxn?
 # encounters [0 or 1]
 enc <- rbinom(N, 1, psi.full) * rbinom(N, 1, p)
@@ -85,10 +72,10 @@ model{
 	for(i in 1:N){
 		if(enc[i] > 0){
 			//the site was occupied, and you detected it
-			target += log_inv_logit(logit_psi) + bernoulli_logit_lpmf(1| logit_p + hypox_p*hypox[i]);
+			target += log_inv_logit(logit_p) + bernoulli_logit_lpmf(1| logit_psi + hypox_p*hypox[i]);
 		}else{
 			//the site was occupied but you didn't detect it & the site was unoccupied
-			target += log_sum_exp(log_inv_logit(logit_psi) + bernoulli_logit_lpmf(0| logit_p + hypox_p*hypox[i]), log1m_inv_logit(logit_psi));
+			target += log_sum_exp(log_inv_logit(logit_p) + bernoulli_logit_lpmf(0| logit_psi + hypox_p*hypox[i]), log1m_inv_logit(logit_p));
 		}
 	}
 }"
@@ -107,7 +94,7 @@ model{
 ###############################  Creating data in list format necessary for Stan ####################################
 occ.stan.dat <- list(N = N,
                      enc = enc,
-                     hypox = bad_yr)
+                     hypox = good_yr)
 
 # Run Stan model 
 occ.stan <- stan(model_code = occ.mod,
@@ -115,7 +102,7 @@ occ.stan <- stan(model_code = occ.mod,
                  data = occ.stan.dat,
                  chains = 4,
                  warmup = 1000,
-                 iter = 2000,
+                 iter = 3000,
                  #init = inits, #may need to be species specific 
                  control = list(adapt_delta = 0.95)) #target acceptance prob.
              
