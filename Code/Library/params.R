4f4d999900dcae8e9aea0a81b59304abcbf7e8c5
McleodMJ <montana.mcleod@oregonstate.edu>
2021-02-16 16:36:59 -0800

    add maturity parameters

--- Code/Library/params.R
+++ Code/Library/params.R
@@ -17,6 +17,8 @@ params <- function(sp){
     f = 0.017,
     fec.const = 0.000002805, #linear to weight
     fec.exp = 3.345,
+    fec.int =1, #1
+    fec.slp = 3,#0 gm-slope p# 314
     M = 0.1165,
     M.sd = 0.0056,
     K = 0.1497,
@@ -24,7 +26,9 @@ params <- function(sp){
     Linf = 47.81,
     Linf.cv = 0.114,
     Rlen = 5.405,
-    Rlen.sd = 0.094 * 5.405)
+    Rlen.sd = 0.094 * 5.405, 
+    Mat.len = 35,  #p. 34
+    Mat.slp = -0.775)# page 314
     
   }
   
@@ -38,6 +42,8 @@ params <- function(sp){
       f = 0.115,
       fec.const = 0.00000276,
       fec.exp = 3.28,
+      fec.int =1,
+      fec.slp = 3,
       M = 0.257,
       M.sd = 0.4384,
       K = 0.0173,
@@ -45,7 +51,9 @@ params <- function(sp){
       Linf = 108.6,
       Linf.cv = 0.06,
       Rlen = 17.2792,
-      Rlen.sd = 0.1436 * 17.28) #cv-length min * mean 
+      Rlen.sd = 0.1436 * 17.28, #cv-length min * mean #page 97
+      Mat.len = 56.7, # p. 97 but 64 on #page 30
+      Mat.slp = -0.269) #p.97
     
   }
   
@@ -59,14 +67,18 @@ params <- function(sp){
       f = 0.0682, #exploitation rate: SPR(msy)
       fec.const = 371200, #fec. intercept
       fec.exp = 63300, #fec. slope
+      fec.int =1, #1 
+      fec.slp = 3, #0 p#207
       M = 0.08, 
       M.sd = NA, # 95 AI: 0.02-.4
       K = 0.11,
       K.sd = 0.003,
       Linf = 33.67,
       Linf.cv = 0.07,
-      Rlen = NA,
-      Rlen.sd = NA)
+      Rlen = 9.25,#page 74
+      Rlen.sd = 0.09 * 9.25, #cv given
+      Mat.len = 20.97 ,  #p. 207
+      Mat.slp = -0.66) #p. 207
     
   }
   
@@ -80,14 +92,18 @@ params <- function(sp){
       f = 0.089, #95 CI: 0.085-0.093
       fec.const = 1.1185e-11,
       fec.exp = 4.59,
+      fec.int =1, #0
+      fec.slp = 3, #3 p# 80
       M = 0.173,
       M.sd = NA,
       K = 0.17,
       K.sd = NA, #literature values used/ do not give SD
       Linf = 52.2,
       Linf.cv = 0.1, #do not give SD or CV - SD estimated at 0.1
-      Rlen = NA,
-      Rlen.sd = NA) 
+      Rlen = 14.689,#page 80
+      Rlen.sd = 0.105 * 14.689,# given as cv
+      Mat.len = 42.49,  #p. 80
+      Mat.slp = -0.401) #guess
    }
   return(Params)
 }
