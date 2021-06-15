af621e655a83abc5a1a170d6be9c3eb2be3cf180
McleodMJ <montana.mcleod@oregonstate.edu>
2021-06-15 15:25:31 -0700

    Particle filter simulation

---
+++ Code/Particle_Filter.R
@@ -0,0 +1,31 @@
+
+#require(ggplot2)
+#require(patchwork)
+setwd("~/Box Sync/McLeod_thesis/Code")
+load("./Results/Expected_Fishing_rate.Rdata") #from the SPR vs fishing values 
+load("../Data/Hist_spp.Rdata") # data made into histogram
+Selex <- read.csv("../Data/Sp.selex.csv")
+WClen <- Selex$Dsole # selectivity for Dsole
+Dsole_f <- Sp_depl[Sp_depl$Species == "Dsole", 3] #expected Dsole fishing rate based on SPR analysis
+
+
+source("./Library/params.R")
+source("./Library/kernmat.R")
+source("./Library/fecmat.R")
+
+source("./Library/IPM.R")
+source("./Library/p.filter.R")
+
+
+Pop_IPM_eq <- IPM("Dsole", Dsole_f, 100, 200, NA, 0) # run IPM once to get to equilibrium 
+Pop_IPM_var <- IPM("Dsole",Dsole_f, 100, 200, Pop_IPM_eq$SAD, 0.01) # run IPM to include recruitment variation and use N0 from first IPM run [N0 is N[,time] from first IPM run]
+Pop_var <- round(Pop_IPM_var$Pop.matrix)
+
+Pfil_Out <- p.filter(Pop_IPM_eq$Pop.matrix, Pop_var, "Dsole", Dsole_f, NA, 200, 100, 100, 0.01, 0.1, 0.1) # particle filter using 1st IPM runs data to initialize the model and 2nd IPM run as the Nact data
+
+par(mfrow=c(1,2))
+plot(colSums(Pfil_Out$Pop), type='l'); plot(colSums(Pop_var), type='l') 
+
+
+
+
