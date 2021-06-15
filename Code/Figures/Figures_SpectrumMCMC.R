3dbc1d8a883309122d344e89009df6ae031bb6d5
McleodMJ <montana.mcleod@oregonstate.edu>
2021-06-15 15:15:33 -0700

    Fourier red-noise significance plot

---
+++ Code/Figures/Figures_SpectrumMCMC.R
@@ -0,0 +1,22 @@
+require(ggplot2)
+
+DOdata <- read.delim("~/Box Sync/McLeod_thesis/Data/DO_Data.txt", comment.char="#")
+load("DO_sims.Rdata")
+
+
+
+ggplot(dat_NA[dat_NA$year %in% 2010:2018, ], aes(Jday, DO, color=year))+
+  geom_line()+
+  scale_fill_brewer(palette = "Accent")+
+  facet_wrap(~year, 4)+
+  geom_hline(yintercept = 1.43, color= "red")+
+  ggtitle("Summertime DO at a bottom mooring off Newport")+
+  guides(color="none")
+
+
+ggplot(DsD, aes(Period, Freq_sq))+
+  geom_line()+
+  geom_line(aes(Period, Signf, color= "red"))+
+  facet_wrap(~Year)+
+  ggtitle('Fourier spectra of DO timeseries, used to generate realistic simulated timeseries for method development')+
+  guides(color="none")
