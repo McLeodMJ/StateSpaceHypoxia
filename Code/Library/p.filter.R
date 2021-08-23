acebfbc1e14de07b78f2227eea39749af2bf573b
McleodMJ <montana.mcleod@oregonstate.edu>
2021-08-23 13:43:21 -0700

    include params as arguments - initialize avg likel w/ zeros to prevent NA bugs

--- Code/Library/p.filter.R
+++ Code/Library/p.filter.R
@@ -24,11 +24,9 @@
 # Sp_selex <- read.csv("../Data/Sp.selex.csv") 
 
 
-####################
-MUST CHANGE SO IT REMOVES THE BH EQUATION AND JUST INCLUDES RECRUITMENT AS A FIXED PARAM & SELEC : WCLEN SHOULD JSUT BE A VECTOR OF MESH SIZES
 #######################################
 
-p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_q, sigma_p){
+p.filter <- function(dat, Nact, hypox, fish, hypox_p, fi, mesh, Q, time, rec_var, cv_q, sigma_p){
   
   # Particle filter: (following Knape & deValpine 2012)
   N <- matrix(NA, nrow = nrow(dat), ncol = time)
@@ -44,7 +42,7 @@ p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_
   # Step 1: Initialize the model 
     # resample for accuracy
     ftmp <- matrix(NA, nrow= time, ncol = Q)
-    Avg.likel <- rep(NA, time) #average of the ftmp
+    Avg.likel <- rep(0, time) #average of the ftmp
     ftmp[1,] <- rep(1, Q)
     Wgt = cumsum(ftmp[1,]/ sum(ftmp[1,]))  
     Rnd = runif(Q)
@@ -61,7 +59,7 @@ p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_
   ## Step 2: simulate encounters for Null or absences based on hypoxia parameter 
   
   #threshold -50
-  occ.full.2 <- inv_logit(hypox * hypox.p) #linear dep. on hypoxia
+  occ.full.2 <- inv_logit(hypox * hypox_p) #linear dep. on hypoxia
   # may want to replace data with simulated pop and set a threshold
   # threshold is blanket greater .5 then it is true. [check the hypox for if larger is 0 or 1]
   
@@ -74,7 +72,6 @@ p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_
   ## Step 3: run model through time T 
     # Create the kernel:
     pars <- params(fish)
-    selc <- pars$selc
     
     meshmax = pars$Linf * 2
     x <- seq(from = 1, to = meshmax, length.out = mesh)
@@ -83,7 +80,7 @@ p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_
   ## size distribution of recruits
   Rvec <- dnorm(x, pars$Rlen, pars$Rlen.sd)  
   
-  K <- kernmat(x, pars, Dsole_f)
+  K <- kernmat(x, pars, fi)
   Fe <- fecmat(x, pars)
   
   for (t in 2:time){
@@ -99,28 +96,29 @@ p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_
       # Beverton-holt density-depedence
       Recruits <- as.vector((4 * pars$steep * exp(pars$R0) * E) / ((pars$S0 * (1 - pars$steep)) + (E * (5 * pars$steep - 1))))
       # Add variation in recruitment 
-      RR <- exp(rnorm(1, mean = log(Recruits) - ((cv * log(Recruits))^2)/2, sd= cv * log(Recruits) ))# change cv to 0 for no variation
       
-      Nf[,t, q] = Nf[,t, q] + (Rvec * RR) + Nrand  # move the model forward for each particle 
+      #RR <- exp(rnorm(1, mean = log(Recruits) - ((cv * log(Recruits))^2)/2, sd= cv * log(Recruits) ))# change cv to 0 for no variation
+      
+      Nf[,t, q] = Nf[,t, q] + rec_var * (Rvec * Recruits) + Nrand  # move the model forward for each particle 
     } # end of Q loop
     Nf[Nf < 0] = 0
     
     # Need to look into WCGBTS Selectivity
-    if (length(selc) == 1){
-       likel <- dpois(matrix(rep(Nact[,t], Q), ncol=Q), Nf[,t,] * dx, log= T) 
+    #if (length(selc) == 1){
+    #   likel <- dpois(matrix(rep(Nact[,t], Q), ncol=Q), Nf[,t,] * dx, log= T) 
        #' mean is intergration of NF to get counts b/c working with densities prior - midpt rule 
+    #  likel[which(!is.finite(likel))] <- 0
+     # ftmp[t,] = colSums(likel) #log-likelihood vector
+     # Avg.likel[t] = mean(ftmp) # if we want to create a vector of likelihoods
+    #} else {
+      WClen <- pars$selc
+      likel <- dpois(Nact[,t], Nf[,t,] * dx * WClen, log= T) 
       likel[which(!is.finite(likel))] <- 0
       ftmp[t,] = colSums(likel) #log-likelihood vector
-      Avg.likel[t] = mean(ftmp) # if we want to create a vector of likelihoods
-    } else {
-      WClen <- selc * dat
-      likel <- dpois(matrix(rep(Nact[,t], Q), ncol=Q), Nf[,t,] * dx * rep(WClen[,Q]), log= T) 
-      likel[which(!is.finite(likel))] <- 0
-      ftmp[t,] = colSums(likel) #log-likelihood vector
-      Avg.likel[t] = mean(ftmp)
+      Avg.likel[t] = mean(ftmp[t,])
       #ftmp(t,:) = sum(log(max(realmin,poisspdf(repmat(Nact(:,t),[1,1,Q]),(NT(t-length(Tpre))*dy*Nf(:,t,:)).*repmat(OKlen(:),[1,1,Q])))));
       # oklen = filter out sizes wouldnt see in the data (btw 0-1: with full selection at 1)
-    }
+   # }
     # if averages over trawl (lognormal) otherwise poisson dpois()
  
    ## Step 4: Resample    
@@ -153,6 +151,6 @@ p.filter <- function(dat, Nact, hypox, fish, hypox.p, fi, mesh, Q, time, cv, cv_
 # Q = 100
 # time = N
 # cv = 0.01
+# rec_var = 0.1
 # cv_q = 0.1
 # sigma_p = 0.1
-# 
